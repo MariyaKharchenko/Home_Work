e5eb1bf62bc424a73c14cd8ca7823acd27a3f5dd
Mariya_Kharchenko <mariyaanatolevna86@yandex.ru>
2023-06-17 12:43:38 +0300

    Sloved task two and three

---
+++ HomeWork2/Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, 
+является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+Console.Write("Введите число от 1 до 7: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0 && number < 8)
+{
+    if (number < 6)
+    {
+        Console.WriteLine("Это будний день");
+    }
+    else
+    {
+        Console.WriteLine("Это выходной!");
+    }
+}
+else
+{
+    Console.WriteLine("Введите число от 1 до 7");
+}
