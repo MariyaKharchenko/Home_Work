0b3bbd32b4a80cdc0ffc295fdf840549df13dc9e
Mariya_Kharchenko <mariyaanatolevna86@yandex.ru>
2023-06-17 12:45:50 +0300

    Sloved task two and three

---
+++ HomeWork2/Task2/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+List<int> ourList = new List<int>() {};
+
+int result = 0;
+
+while (number > 0)
+{
+    ourList.Insert(0, number%10);
+    number = number/10;
+    result ++;
+
+}
+
+
+if (result > 2)
+{
+    Console.WriteLine(ourList[2]);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
