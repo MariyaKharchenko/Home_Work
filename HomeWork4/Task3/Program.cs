eb97c70ae67acd5b4ee1aa4a83fd8d7ebedf7e12
Mariya_Kharchenko <mariyaanatolevna86@yandex.ru>
2023-06-23 14:37:35 +0300

    Sloved Task3

---
+++ HomeWork4/Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+void FillArray(int[]array){
+    int length = array.Length;
+    for (int i = 0; i < length; i++){
+        array[i] = new Random().Next(1, 100);
+    }
+    Console.WriteLine($"[{string.Join(", ", array)}]");
+}
+
+int[] arr = new int[8];
+FillArray(arr);
+FillArray(arr);
+FillArray(arr);
