4a63d50de92eb6e96e14af5e676e9bb9954dcbc4
Mariya_Kharchenko <mariyaanatolevna86@yandex.ru>
2023-07-09 14:34:57 +0300

    Sloved Task1

---
+++ HomeWork9/Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задайте значение N. 
+Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+int GetNumber(string description){
+    Console.Write($"{description}: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+void ShowsNumbers(int n, int min=1){
+    Console.WriteLine(n);
+    if (n>min) ShowsNumbers(n-1);
+}
+
+int n = GetNumber("Введите число n");
+ShowsNumbers(n);
